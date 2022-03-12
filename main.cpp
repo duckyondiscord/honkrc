eec85b83b5d6198dca7ca796d06fbe0c94220c6d
ducky <94759537+duckyondiscord@users.noreply.github.com>
2022-03-12 11:15:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <thread> // This is for later use when we add multithreading support
+#include <thread>
 #include <string>
+#include <unistd.h>
 
-std::string startSrv(std::string exec)
+int startSrv(std::string exec) // Function to start the services in the specified directory
 {
-	exec = "sh " + exec + "/*";
-	system(exec.c_str());
-	return exec;
+	exec = "sh" + exec + "/*";
+	return system(exec.c_str());;
 }
 
 int main(int argc, char** argv)
@@ -15,11 +15,18 @@ int main(int argc, char** argv)
 	  if (argc > 1)
 	  {
 	  	std::string arg1(argv[1]);
-	  	startSrv(arg1);
+		int returnvalue;
+		returnvalue = startSrv(arg1);
+		
+		if(returnvalue != 0)
+		{
+			// Let the user know whether there was an error while running the services
+			std::cout << "An error occured while running services, EXITING!";
+		}
 	  }
 	else 
 	{
-		std::cout << "Expected an argument, EXITING!\n";
+		std::cout << "Expected an argument, EXITING!\n"; // Output an error when there's no argument instead of crashing outright.
 		return 1;
 	}
 }
